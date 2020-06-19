bcf4524839b752c63486eeef7978194ef0aea5ab
paul.wegiel <paul-wegiel@orange.fr>
2020-06-19 09:29:31 -0400

    Jumper 2 phases

---
+++ optimization_biorbdOptim/jumper2phases.py
@@ -0,0 +1,239 @@
+from time import time
+
+import biorbd
+
+from biorbd_optim import (
+    Instant,
+    OptimalControlProgram,
+    Constraint,
+    Objective,
+    ProblemType,
+    BidirectionalMapping,
+    Mapping,
+    Bounds,
+    QAndQDotBounds,
+    InitialConditions,
+    ShowResult,
+    Data,
+)
+
+def from_2contacts_to_1(ocp, nlp, t, x, u, p):
+    return ocp.nlp[0]["contact_forces_func"](x[0], u[0], p)[[2, 5], -1]
+
+# def custom_func_anatomical_constraint(ocp, nlp, t, x, u, p):
+#     val = x[0][7:14]
+#     return val
+
+def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=True):
+    # --- Options --- #
+    # Model path
+    biorbd_model = [biorbd.Model(elt) for elt in model_path]
+
+    nb_phases = len(biorbd_model)
+    # torque_activation_min, torque_activation_max, torque_activation_init = -1, 1, 0
+    torque_min, torque_max, torque_init = -1000, 1000, 0
+
+
+    if use_symmetry:
+        q_mapping = BidirectionalMapping(
+            Mapping([0, 1, 2, -1, 3, -1, 3, 4, 5, 6, 4, 5, 6], [5]), Mapping([0, 1, 2, 4, 7, 8, 9])
+        )
+        q_mapping = q_mapping, q_mapping
+        tau_mapping = BidirectionalMapping(
+            Mapping([-1, -1, -1, -1, 0, -1, 0, 1, 2, 3, 1, 2, 3], [5]), Mapping([4, 7, 8, 9])
+        )
+        tau_mapping = tau_mapping, tau_mapping
+
+    else:
+        q_mapping = BidirectionalMapping(
+            Mapping([i for i in range(biorbd_model[0].nbQ())]), Mapping([i for i in range(biorbd_model[0].nbQ())])
+        )
+        q_mapping = q_mapping, q_mapping
+        tau_mapping = q_mapping
+
+    # Add objective functions
+    objective_functions = (
+        (
+            # {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": -1 / 100},
+        ),
+        (
+            {"type": Objective.Mayer.MINIMIZE_PREDICTED_COM_HEIGHT, "weight": -1},
+            # {"type": Objective.Lagrange.MINIMIZE_ALL_CONTROLS, "weight": 1 / 100},
+            # {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": -1 / 100},
+        ),
+    )
+
+    # Dynamics
+    # problem_type = (
+    #     {"type": ProblemType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT},
+    #     {"type": ProblemType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT},
+    # )
+    problem_type = (
+        {"type": ProblemType.TORQUE_DRIVEN_WITH_CONTACT},
+        {"type": ProblemType.TORQUE_DRIVEN_WITH_CONTACT},
+    )
+
+    constraints_first_phase = []
+    constraints_second_phase = []
+
+    # Positivity constraints of the normal component of the reaction forces
+    contact_axes = (1, 2, 4, 5)
+    for i in contact_axes:
+        constraints_first_phase.append(
+            {
+                "type": Constraint.CONTACT_FORCE_INEQUALITY,
+                "direction": "GREATER_THAN",
+                "instant": Instant.ALL,
+                "contact_force_idx": i,
+                "boundary": 0,
+            }
+        )
+    contact_axes = (1, 3)
+    for i in contact_axes:
+        constraints_second_phase.append(
+            {
+                "type": Constraint.CONTACT_FORCE_INEQUALITY,
+                "direction": "GREATER_THAN",
+                "instant": Instant.ALL,
+                "contact_force_idx": i,
+                "boundary": 0,
+            }
+        )
+
+    # Non-slipping constraints
+    # N.B.: Application on only one of the two feet is sufficient, as the slippage cannot occurs on only one foot.
+    constraints_first_phase.append(
+        {
+            "type": Constraint.NON_SLIPPING,
+            "instant": Instant.ALL,
+            "normal_component_idx": (1, 2),
+            "tangential_component_idx": 0,
+            "static_friction_coefficient": 0.5,
+        }
+    )
+    constraints_second_phase.append(
+        {
+            "type": Constraint.NON_SLIPPING,
+            "instant": Instant.ALL,
+            "normal_component_idx": 1,
+            "tangential_component_idx": 0,
+            "static_friction_coefficient": 0.5,
+        }
+    )
+
+    # Custom constraints for contact forces at transitions
+    constraints_second_phase.append(
+        {"type": Constraint.CUSTOM, "function": from_2contacts_to_1, "instant": Instant.START}
+    )
+
+    if not use_symmetry:
+        first_dof = (3, 4, 7, 8, 9)
+        second_dof = (5, 6, 10, 11, 12)
+        coef = (-1, 1, 1, 1, 1)
+        for i in range(len(first_dof)):
+            for elt in [
+                constraints_first_phase,
+                constraints_second_phase,
+            ]:
+                elt.append(
+                    {
+                        "type": Constraint.PROPORTIONAL_STATE,
+                        "instant": Instant.ALL,
+                        "first_dof": first_dof[i],
+                        "second_dof": second_dof[i],
+                        "coef": coef[i],
+                    }
+                )
+    constraints = (
+        constraints_first_phase,
+        constraints_second_phase,
+    )
+    # for i, constraints_phase in enumerate(constraints):
+    #     constraints_phase.append({"type": Constraint.TIME_CONSTRAINT, "minimum": time_min[i], "maximum": time_max[i]})
+
+    # Path constraint
+    if use_symmetry:
+        nb_q = q_mapping[0].reduce.len
+        nb_qdot = nb_q
+        pose_at_first_node = [0, 0, -0.5336, 1.4, 0.8, -0.9, 0.47]
+    else:
+        nb_q = q_mapping[0].reduce.len
+        nb_qdot = nb_q
+        pose_at_first_node = [0, 0, -0.5336, 0, 1.4, 0, 1.4, 0.8, -0.9, 0.47, 0.8, -0.9, 0.47]
+
+    # Initialize X_bounds
+    X_bounds = [QAndQDotBounds(biorbd_model[i], all_generalized_mapping=q_mapping[i]) for i in range(nb_phases)]
+    X_bounds[0].min[:, 0] = pose_at_first_node + [0] * nb_qdot
+    X_bounds[0].max[:, 0] = pose_at_first_node + [0] * nb_qdot
+
+    # Initial guess
+    X_init = [InitialConditions(pose_at_first_node + [0] * nb_qdot) for i in range(nb_phases)]
+
+    # Define control path constraint
+    U_bounds = [
+        Bounds(
+            # min_bound=[torque_activation_min] * tau_m.reduce.len, max_bound=[torque_activation_max] * tau_m.reduce.len
+            min_bound=[torque_min] * tau_m.reduce.len, max_bound=[torque_max] * tau_m.reduce.len
+        )
+        for tau_m in tau_mapping
+    ]
+
+    # U_init = [InitialConditions([torque_activation_init] * tau_m.reduce.len) for tau_m in tau_mapping]
+    U_init = [InitialConditions([torque_init] * tau_m.reduce.len) for tau_m in tau_mapping]
+    # ------------- #
+
+    return OptimalControlProgram(
+        biorbd_model,
+        problem_type,
+        number_shooting_points,
+        phase_time,
+        X_init,
+        U_init,
+        X_bounds,
+        U_bounds,
+        objective_functions=objective_functions,
+        constraints=constraints,
+        q_mapping=q_mapping,
+        q_dot_mapping=q_mapping,
+        tau_mapping=tau_mapping,
+    )
+
+
+def run_and_save_ocp(model_path, phase_time, number_shooting_points):
+    ocp = prepare_ocp(
+        model_path=model_path, phase_time=phase_time, number_shooting_points=number_shooting_points, use_symmetry=True
+    )
+    sol = ocp.solve(options_ipopt={"hessian_approximation": "limited-memory", "max_iter": 1000}, show_online_optim=True)
+
+    OptimalControlProgram.save(ocp, sol, "../Results/jumper2phases_sol")
+
+
+if __name__ == "__main__":
+    model_path = (
+        "../models/jumper2contacts.bioMod",
+        "../models/jumper1contacts.bioMod",
+    )
+    time_min = [0.05, 0.1]
+    time_max = [0.5, 0.5]
+    phase_time = [0.4, 0.2]
+    number_shooting_points = [20, 20]
+
+    tic = time()
+    run_and_save_ocp(model_path, phase_time=phase_time, number_shooting_points=number_shooting_points)
+    ocp, sol = OptimalControlProgram.load("../Results/jumper2phases_sol.bo")
+
+    # ocp = prepare_ocp(model_path=model_path, phase_time=phase_time, number_shooting_points=number_shooting_points, use_symmetry=True)
+    # sol = ocp.solve(show_online_optim=False, options_ipopt={"hessian_approximation": "limited-memory", "max_iter": 2})
+
+    # --- Show results --- #
+    # param = Data.get_data(ocp, sol["x"], get_states=False, get_controls=False, get_parameters=True)
+    # print(
+    #     f"The optimized phases times are: {param['time'][0, 0]}s and {param['time'][1, 0]}s."
+    # )
+
+    toc = time() - tic
+    print(f"Time to solve : {toc}sec")
+
+    result = ShowResult(ocp, sol)
+    result.graphs()
+    result.animate(nb_frames=61)
