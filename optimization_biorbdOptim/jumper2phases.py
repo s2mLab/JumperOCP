5d1369fdfaeb4023014d9ab4efca76913a634a80
paul.wegiel <paul-wegiel@orange.fr>
2020-07-20 20:59:18 -0400

    Cleaning 2-phase jumper and adjusting settings

--- optimization_biorbdOptim/jumper2phases.py
+++ optimization_biorbdOptim/jumper2phases.py
@@ -53,6 +53,8 @@ def CoM_dot_Z_last_node_positivity(ocp, nlp, t, x, u, p):
     return CoM_dot[2]
 
 
+
+
 def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=True, use_actuators=True):
     # --- Options --- #
     # Model path
@@ -60,9 +62,9 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
 
     nb_phases = len(biorbd_model)
     if use_actuators:
-        torque_activation_min, torque_activation_max, torque_activation_init = -1, 1, 0
+        tau_min, tau_max, tau_init = -1, 1, 0
     else:
-        torque_min, torque_max, torque_init = -500, 500, 0
+        tau_min, tau_max, tau_init = -500, 500, 0
 
     if use_symmetry:
         q_mapping = BidirectionalMapping(
@@ -87,70 +89,28 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
 
     # Dynamics
     dynamics = DynamicsTypeList()
-    dynamics.add(DynamicsType.TORQUE_DRIVEN_WITH_CONTACT)
-    dynamics.add(DynamicsType.TORQUE_DRIVEN_WITH_CONTACT)
+    if use_actuators:
+        dynamics.add(DynamicsType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT)
+        dynamics.add(DynamicsType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT)
+    else:
+        dynamics.add(DynamicsType.TORQUE_DRIVEN_WITH_CONTACT)
+        dynamics.add(DynamicsType.TORQUE_DRIVEN_WITH_CONTACT)
 
-    # dynamics.add(DynamicsType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT)
-    # dynamics.add(DynamicsType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT)
-
-    # Constraints
+    # --- Constraints --- #
     constraints = ConstraintList()
-    constraints.add(Constraint.ALIGN_MARKERS, instant=Instant.START, first_marker_idx=0, second_marker_idx=1, phase=0)
-
-    # constraints_first_phase = []
-    # constraints_second_phase = []
 
     # Positivity constraints of the normal component of the reaction forces
     contact_axes = (1, 2, 4, 5)
     for i in contact_axes:
-        # constraints_first_phase.append(
-        #     {
-        #         "type": Constraint.CONTACT_FORCE_INEQUALITY,
-        #         "direction": "GREATER_THAN",
-        #         "instant": Instant.ALL,
-        #         "contact_force_idx": i,
-        #         "boundary": 0,
-        #     }
-        # )
         constraints.add(Constraint.CONTACT_FORCE_INEQUALITY, phase=0, direction="GREATER_THAN", instant=Instant.ALL, contact_force_idx=i, boundary=0)
     contact_axes = (1, 3)
     for i in contact_axes:
-        # constraints_second_phase.append(
-        #     {
-        #         "type": Constraint.CONTACT_FORCE_INEQUALITY,
-        #         "direction": "GREATER_THAN",
-        #         "instant": Instant.ALL,
-        #         "contact_force_idx": i,
-        #         "boundary": 0,
-        #     }
-        # )
         constraints.add(Constraint.CONTACT_FORCE_INEQUALITY, phase=1, direction="GREATER_THAN", instant=Instant.ALL, contact_force_idx=i, boundary=0)
-
 
     # Non-slipping constraints
     # N.B.: Application on only one of the two feet is sufficient, as the slippage cannot occurs on only one foot.
-    # constraints_first_phase.append(
-    #     {
-    #         "type": Constraint.NON_SLIPPING,
-    #         "instant": Instant.ALL,
-    #         "normal_component_idx": (1, 2),
-    #         "tangential_component_idx": 0,
-    #         "static_friction_coefficient": 0.5,
-    #     }
-    # )
     constraints.add(Constraint.NON_SLIPPING, phase=0, instant=Instant.ALL, normal_component_idx=(1, 2), tangential_component_idx=0, static_friction_coefficient=0.5)
-
-    # constraints_second_phase.append(
-    #     {
-    #         "type": Constraint.NON_SLIPPING,
-    #         "instant": Instant.ALL,
-    #         "normal_component_idx": 1,
-    #         "tangential_component_idx": 0,
-    #         "static_friction_coefficient": 0.5,
-    #     }
-    # )
     constraints.add(Constraint.NON_SLIPPING, phase=1, instant=Instant.ALL, normal_component_idx=1, tangential_component_idx=0, static_friction_coefficient=0.5)
-
 
     # Custom constraints for contact forces at transitions
     # constraints_second_phase.append(
@@ -160,6 +120,7 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
     # Custom constraints for positivity of CoM_dot on z axis just before the take-off
     constraints.add(CoM_dot_Z_last_node_positivity, phase=1, instant=Instant.END, min_bound=0, max_bound=np.inf)
 
+    # TODO: Make it works also with no symmetry (adapt to refactor)
     # if not use_symmetry:
     #     first_dof = (3, 4, 7, 8, 9)
     #     second_dof = (5, 6, 10, 11, 12)
@@ -183,10 +144,12 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
     #     constraints_first_phase,
     #     constraints_second_phase,
     # )
-    # for i, constraints_phase in enumerate(constraints):
-    #     constraints_phase.append({"type": Constraint.TIME_CONSTRAINT, "minimum": time_min[i], "maximum": time_max[i]})
 
-    # Path constraint
+    # # Time constraint
+    # for i in range(nb_phases):
+    #     constraints.add(Constraint.TIME_CONSTRAINT, phase=i, minimum=time_min[i], maximum=time_max[i])
+
+    # --- Path constraints --- #
     if use_symmetry:
         nb_q = q_mapping[0].reduce.len
         nb_qdot = nb_q
@@ -203,12 +166,10 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
     x_bounds[0].min[:, 0] = pose_at_first_node + [0] * nb_qdot
     x_bounds[0].max[:, 0] = pose_at_first_node + [0] * nb_qdot
 
-    # X_bounds = [QAndQDotBounds(biorbd_model[i], all_generalized_mapping=q_mapping[i]) for i in range(nb_phases)]
-    # X_bounds[0].min[:, 0] = pose_at_first_node + [0] * nb_qdot
-    # X_bounds[0].max[:, 0] = pose_at_first_node + [0] * nb_qdot
-
-    # X_bounds = ProblemType.slicing_bounds("q", X_bounds)
-    # q_bounds = X_bounds[0][:nq]
+    # # Initial guess for states (Interpolation type is CONSTANT)
+    # x_init = InitialConditionsList()
+    # for i in range(nb_phases):
+    #     x_init.add(pose_at_first_node + [0] * nb_qdot)
 
     # Initial guess for states (Interpolation type is CONSTANT for 1st phase and SPLINE with 3 key positions for 2nd phase)
     x_init = InitialConditionsList()
@@ -220,40 +181,14 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
     key_positions = np.hstack((p0, p_flex, p_end))
     x_init.add(key_positions, t=t_spline, interpolation=InterpolationType.SPLINE)       # x_init phase 1 type SPLINE
 
-    # Define control path constraint (
-    if use_actuators:
-        u_bounds = BoundsList()
-        for tau_m in tau_mapping:
-            u_bounds.add([[torque_activation_min] * tau_m.reduce.len, [torque_activation_max] * tau_m.reduce.len], interpolation=InterpolationType.CONSTANT)    # This precision of the CONSTANT type is for informative purposes only
+    # Define control path constraint
+    u_bounds = BoundsList()
+    for tau_m in tau_mapping:
+        u_bounds.add([[tau_min] * tau_m.reduce.len, [tau_max] * tau_m.reduce.len], interpolation=InterpolationType.CONSTANT)    # This precision of the CONSTANT type is for informative purposes only
 
-    # if use_actuators:
-    #     U_bounds = [
-    #         Bounds(
-    #             min_bound=[torque_activation_min] * tau_m.reduce.len,
-    #             max_bound=[torque_activation_max] * tau_m.reduce.len,
-    #         )
-    #         for tau_m in tau_mapping
-    #     ]
-    #     U_init = [InitialConditions([torque_activation_init] * tau_m.reduce.len) for tau_m in tau_mapping]
-
-        u_init = InitialConditionsList()
-        for tau_m in tau_mapping:
-            u_init.add([torque_activation_init] * tau_m.reduce.len)     # Interpolation type is CONSTANT (default value)
-
-    else:
-        u_bounds = BoundsList()
-        for tau_m in tau_mapping:
-            u_bounds.add([[torque_min] * tau_m.reduce.len, [torque_max] * tau_m.reduce.len], interpolation=InterpolationType.CONSTANT)  # This precision of the CONSTANT type is for informative purposes only
-        u_init = InitialConditionsList()
-        for tau_m in tau_mapping:
-            u_init.add([torque_init] * tau_m.reduce.len)  # Interpolation type is CONSTANT (default value)
-
-    # else:
-    #     U_bounds = [
-    #         Bounds(min_bound=[torque_min] * tau_m.reduce.len, max_bound=[torque_max] * tau_m.reduce.len)
-    #         for tau_m in tau_mapping
-    #     ]
-    #     U_init = [InitialConditions([torque_init] * tau_m.reduce.len) for tau_m in tau_mapping]
+    u_init = InitialConditionsList()
+    for tau_m in tau_mapping:
+        u_init.add([tau_init] * tau_m.reduce.len)     # Interpolation type is CONSTANT (default value)
 
     # ------------- #
 
@@ -271,6 +206,7 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
         q_mapping=q_mapping,
         q_dot_mapping=q_mapping,
         tau_mapping=tau_mapping,
+        nb_threads=2,
     )
 
 
@@ -326,10 +262,10 @@ if __name__ == "__main__":
         "../models/jumper2contacts.bioMod",
         "../models/jumper1contacts.bioMod",
     )
-    time_min = [0.1, 0.3]
-    time_max = [0.4, 0.5]
-    phase_time = [0.2, 0.32]
-    number_shooting_points = [20, 20]
+    # time_min = [0.1, 0.2]
+    # time_max = [0.5, 1]
+    phase_time = [0.2, 0.4]
+    number_shooting_points = [30, 30]
 
     tic = time()
     # run_and_save_ocp(model_path, phase_time=phase_time, number_shooting_points=number_shooting_points)
@@ -340,7 +276,7 @@ if __name__ == "__main__":
         phase_time=phase_time,
         number_shooting_points=number_shooting_points,
         use_symmetry=True,
-        use_actuators=False,
+        use_actuators=True,
     )
     ocp.add_plot(
         "CoM", lambda x, u, p: plot_CoM(x, "../models/jumper2contacts.bioMod"), phase_number=0, plot_type=PlotType.PLOT
@@ -361,9 +297,9 @@ if __name__ == "__main__":
         plot_type=PlotType.PLOT,
     )
 
-    sol = ocp.solve(show_online_optim=True, solver_options={"hessian_approximation": "exact", "max_iter": 20})
+    sol = ocp.solve(show_online_optim=True, solver_options={"hessian_approximation": "exact", "max_iter": 1000})
 
-    # --- Show results --- #
+    #--- Show results --- #
     # param = Data.get_data(ocp, sol["x"], get_states=False, get_controls=False, get_parameters=True)
     # print(
     #     f"The optimized phases times are: {param['time'][0, 0]}s and {param['time'][1, 0]}s."
@@ -373,5 +309,5 @@ if __name__ == "__main__":
     print(f"Time to solve : {toc}sec")
 
     result = ShowResult(ocp, sol)
-    result.graphs(adapt_graph_size_to_bounds=False)
+    result.graphs(adapt_graph_size_to_bounds=True)
     result.animate(nb_frames=61)
