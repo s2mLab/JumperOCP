82663abd5832707a2305b772d3df41e42eb7d293
paul.wegiel <paul-wegiel@orange.fr>
2020-07-20 21:00:13 -0400

    Adapt 1-phase jumper with recent changes in BiorbdOptim and my other ocp

--- optimization_biorbdOptim/jumper1phase.py
+++ optimization_biorbdOptim/jumper1phase.py
@@ -1,18 +1,24 @@
 from time import time
 
+import numpy as np
 import biorbd
 
 from biorbd_optim import (
     Instant,
     OptimalControlProgram,
+    ConstraintList,
     Constraint,
     Objective,
-    ProblemType,
+    ObjectiveList,
+    DynamicsTypeList,
+    DynamicsType,
     BidirectionalMapping,
     Mapping,
-    Bounds,
+    BoundsList,
     QAndQDotBounds,
     InitialConditions,
+    InitialConditionsList,
+    InterpolationType,
     ShowResult,
     Data,
     PlotType,
@@ -27,16 +33,38 @@ from biorbd_optim import (
 #     val = x[0][7:14]
 #     return val
 
+def CoM_dot_Z_last_node_positivity(ocp, nlp, t, x, u, p):
+    from casadi import Function, MX
+    q_reduced = x[0][:nlp["nbQ"]]
+    qdot_reduced = x[0][nlp["nbQ"]:]
+
+    q_mapping = BidirectionalMapping(
+        Mapping([0, 1, 2, -1, 3, -1, 3, 4, 5, 6, 4, 5, 6], [5]), Mapping([0, 1, 2, 4, 7, 8, 9])
+    )
+    q_expanded = q_mapping.expand.map(q_reduced)
+    qdot_expanded = q_mapping.expand.map(qdot_reduced)
+
+    q_sym = MX.sym("q", q_expanded.size()[0], 1)
+    qdot_sym = MX.sym("q_dot", qdot_expanded.size()[0], 1)
+    CoM_dot_func = Function(
+        "Compute_CoM_dot", [q_sym, qdot_sym], [nlp["model"].CoMdot(q_sym, qdot_sym).to_mx()], ["q", "q_dot"], ["CoM_dot"],
+    ).expand()
+    CoM_dot = CoM_dot_func(q_expanded, qdot_expanded)
+    return CoM_dot[2]
+
+
+
 
 def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=True, use_actuators=True):
     # --- Options --- #
     # Model path
-    biorbd_model = biorbd.Model(model_path)
+    biorbd_model = [biorbd.Model(elt) for elt in model_path]
 
+    nb_phases = len(biorbd_model)
     if use_actuators:
-        torque_activation_min, torque_activation_max, torque_activation_init = -1, 1, 0
+        tau_min, tau_max, tau_init = -1, 1, 0
     else:
-        torque_min, torque_max, torque_init = -500, 500, 0
+        tau_min, tau_max, tau_init = -500, 500, 0
 
     if use_symmetry:
         q_mapping = BidirectionalMapping(
@@ -48,112 +76,133 @@ def prepare_ocp(model_path, phase_time, number_shooting_points, use_symmetry=Tru
         )
         tau_mapping = tau_mapping
 
+    else:
+        q_mapping = BidirectionalMapping(
+            Mapping([i for i in range(biorbd_model[0].nbQ())]), Mapping([i for i in range(biorbd_model[0].nbQ())])
+        )
+        q_mapping = q_mapping
+        tau_mapping = q_mapping
+
     # Add objective functions
-    objective_functions = (
-        {"type": Objective.Mayer.MINIMIZE_PREDICTED_COM_HEIGHT, "weight": -1},
-        # {"type": Objective.Lagrange.MINIMIZE_ALL_CONTROLS, "weight": 1 / 100},
-        # {"type": Objective.Lagrange.MINIMIZE_ALL_CONTROLS, "weight": -1 / 100},
-    )
+    objective_functions = ObjectiveList()
+    objective_functions.add(Objective.Mayer.MINIMIZE_PREDICTED_COM_HEIGHT, weight=-1, phase=0)
 
     # Dynamics
-    # problem_type = (
-    #     {"type": ProblemType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT},
-    # )
-    problem_type = ({"type": ProblemType.TORQUE_DRIVEN_WITH_CONTACT},)
-    constraints_second_phase = []
+    dynamics = DynamicsTypeList()
+    if use_actuators:
+        dynamics.add(DynamicsType.TORQUE_ACTIVATIONS_DRIVEN_WITH_CONTACT)
+    else:
+        dynamics.add(DynamicsType.TORQUE_DRIVEN_WITH_CONTACT)
+
+    # --- Constraints --- #
+    constraints = ConstraintList()
 
     # Positivity constraints of the normal component of the reaction forces
     contact_axes = (1, 2, 4, 5)
     for i in contact_axes:
-        constraints_second_phase.append(
-            {
-                "type": Constraint.CONTACT_FORCE_INEQUALITY,
-                "direction": "GREATER_THAN",
-                "instant": Instant.ALL,
-                "contact_force_idx": i,
-                "boundary": 0,
-            }
-        )
+        constraints.add(Constraint.CONTACT_FORCE_INEQUALITY, phase=0, direction="GREATER_THAN", instant=Instant.ALL, contact_force_idx=i, boundary=0)
 
     # Non-slipping constraints
     # N.B.: Application on only one of the two feet is sufficient, as the slippage cannot occurs on only one foot.
-    # constraints_first_phase.append(
-    #     {
-    #         "type": Constraint.NON_SLIPPING,
-    #         "instant": Instant.ALL,
-    #         "normal_component_idx": (1, 2),
-    #         "tangential_component_idx": 0,
-    #         "static_friction_coefficient": 0.5,
-    #     }
+    constraints.add(Constraint.NON_SLIPPING, phase=0, instant=Instant.ALL, normal_component_idx=(1, 2), tangential_component_idx=0, static_friction_coefficient=0.5)
+
+    # Custom constraints for contact forces at transitions
+    # constraints_second_phase.append(
+    #     {"type": Constraint.CUSTOM, "function": from_2contacts_to_1, "instant": Instant.START}
     # )
-    constraints_second_phase.append(
-        {
-            "type": Constraint.NON_SLIPPING,
-            "instant": Instant.ALL,
-            "normal_component_idx": 1,
-            "tangential_component_idx": 0,
-            "static_friction_coefficient": 0.5,
-        }
-    )
 
-    constraints = (constraints_second_phase,)
+    # Custom constraints for positivity of CoM_dot on z axis just before the take-off
+    constraints.add(CoM_dot_Z_last_node_positivity, phase=0, instant=Instant.END, min_bound=0, max_bound=np.inf)
 
-    # for i, constraints_phase in enumerate(constraints):
-    #     constraints_phase.append({"type": Constraint.TIME_CONSTRAINT, "minimum": time_min, "maximum": time_max})
+    # TODO: Make it works also with no symmetry (adapt to refactor)
+    # if not use_symmetry:
+    #     first_dof = (3, 4, 7, 8, 9)
+    #     second_dof = (5, 6, 10, 11, 12)
+    #     coef = (-1, 1, 1, 1, 1)
+    #     for i in range(len(first_dof)):
+    #         for elt in [
+    #             constraints_first_phase,
+    #             constraints_second_phase,
+    #         ]:
+    #             elt.append(
+    #                 {
+    #                     "type": Constraint.PROPORTIONAL_STATE,
+    #                     "instant": Instant.ALL,
+    #                     "first_dof": first_dof[i],
+    #                     "second_dof": second_dof[i],
+    #                     "coef": coef[i],
+    #                 }
+    #             )
 
-    # Path constraint
+    # constraints = (
+    #     constraints_first_phase,
+    #     constraints_second_phase,
+    # )
+
+    # # Time constraint
+    # for i in range(nb_phases):
+    #     constraints.add(Constraint.TIME_CONSTRAINT, phase=i, minimum=time_min[i], maximum=time_max[i])
+
+    # --- Path constraints --- #
     if use_symmetry:
         nb_q = q_mapping.reduce.len
         nb_qdot = nb_q
         pose_at_first_node = [0, 0, -0.5336, 1.4, 0.8, -0.9, 0.47]
+    else:
+        nb_q = q_mapping.reduce.len
+        nb_qdot = nb_q
+        pose_at_first_node = [0, 0, -0.5336, 0, 1.4, 0, 1.4, 0.8, -0.9, 0.47, 0.8, -0.9, 0.47]
 
-    # Initialize X_bounds
-    X_bounds = QAndQDotBounds(biorbd_model, all_generalized_mapping=q_mapping)
-    X_bounds.min[:, 0] = pose_at_first_node + [0] * nb_qdot
-    X_bounds.max[:, 0] = pose_at_first_node + [0] * nb_qdot
+    # Initialize x_bounds (Interpolation type is CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT)
+    x_bounds = BoundsList()
+    for i in range(nb_phases):
+        x_bounds.add(QAndQDotBounds(biorbd_model[i], all_generalized_mapping=q_mapping))
+    x_bounds[0].min[:, 0] = pose_at_first_node + [0] * nb_qdot
+    x_bounds[0].max[:, 0] = pose_at_first_node + [0] * nb_qdot
 
-    # X_bounds = ProblemType.slicing_bounds("q", X_bounds)
-    # q_bounds = X_bounds[0][:nq]
+    # # Initial guess for states (Interpolation type is CONSTANT)
+    # x_init = InitialConditionsList()
+    # for i in range(nb_phases):
+    #     x_init.add(pose_at_first_node + [0] * nb_qdot)
 
-    # Initial guess
-    X_init = InitialConditions(pose_at_first_node + [0] * nb_qdot)
+    # Initial guess for states (Interpolation type is CONSTANT for 1st phase and SPLINE with 3 key positions for 2nd phase)
+    x_init = InitialConditionsList()
+    t_spline = np.hstack((0, 0.34, phase_time))
+    p0 = np.array([pose_at_first_node + [0] * nb_qdot]).T
+    p_flex = np.array([[-0.12, -0.23, -1.10, 1.85, 2.06, -1.67, 0.55, 0, 0, 0, 0, 0, 0, 0]]).T
+    p_end = p0
+    key_positions = np.hstack((p0, p_flex, p_end))
+    x_init.add(key_positions, t=t_spline, interpolation=InterpolationType.SPLINE)       # x_init phase 1 type SPLINE
 
     # Define control path constraint
-    if use_actuators:
-        U_bounds = [
-            Bounds(
-                min_bound=[torque_activation_min] * tau_mapping.reduce.len,
-                max_bound=[torque_activation_max] * tau_mapping.reduce.len,
-            )
-        ]
-        U_init = [InitialConditions([torque_activation_init] * tau_mapping.reduce.len)]
-    else:
-        U_bounds = [
-            Bounds(min_bound=[torque_min] * tau_mapping.reduce.len, max_bound=[torque_max] * tau_mapping.reduce.len)
-        ]
-        U_init = [InitialConditions([torque_init] * tau_mapping.reduce.len)]
+    u_bounds = BoundsList()
+    u_bounds.add([[tau_min] * tau_mapping.reduce.len, [tau_max] * tau_mapping.reduce.len], interpolation=InterpolationType.CONSTANT)    # This precision of the CONSTANT type is for informative purposes only
+
+    u_init = InitialConditionsList()
+    u_init.add([tau_init] * tau_mapping.reduce.len)     # Interpolation type is CONSTANT (default value)
 
     # ------------- #
 
     return OptimalControlProgram(
         biorbd_model,
-        problem_type,
+        dynamics,
         number_shooting_points,
         phase_time,
-        X_init,
-        U_init,
-        X_bounds,
-        U_bounds,
-        objective_functions=objective_functions,
-        constraints=constraints,
+        x_init,
+        u_init,
+        x_bounds,
+        u_bounds,
+        objective_functions,
+        constraints,
         q_mapping=q_mapping,
         q_dot_mapping=q_mapping,
         tau_mapping=tau_mapping,
+        nb_threads=2,
     )
 
 
-def plot_CoM(x):
-    m = biorbd.Model("../models/jumper2contacts.bioMod")
+def plot_CoM(x, model_path):
+    m = biorbd.Model(model_path)
     q_mapping = BidirectionalMapping(
         Mapping([0, 1, 2, -1, 3, -1, 3, 4, 5, 6, 4, 5, 6], [5]), Mapping([0, 1, 2, 4, 7, 8, 9])
     )
@@ -168,8 +217,8 @@ def plot_CoM(x):
     return CoM[2]
 
 
-def plot_CoM_dot(x):
-    m = biorbd.Model("../models/jumper2contacts.bioMod")
+def plot_CoM_dot(x, model_path):
+    m = biorbd.Model(model_path)
     q_mapping = BidirectionalMapping(
         Mapping([0, 1, 2, -1, 3, -1, 3, 4, 5, 6, 4, 5, 6], [5]), Mapping([0, 1, 2, 4, 7, 8, 9])
     )
@@ -191,19 +240,21 @@ def plot_CoM_dot(x):
 
 # def run_and_save_ocp(model_path, phase_time, number_shooting_points):
 #     ocp = prepare_ocp(
-#         model_path=model_path, phase_time=phase_time, number_shooting_points=number_shooting_points, use_symmetry=True, use_actuators=True
+#         model_path=model_path, phase_time=phase_time, number_shooting_points=number_shooting_points, use_symmetry=True
 #     )
 #
-#     sol = ocp.solve(options_ipopt={"hessian_approximation": "exact", "max_iter": 1000}, show_online_optim=False)
+#     sol = ocp.solve(options_ipopt={"hessian_approximation": "limited-memory", "max_iter": 1000}, show_online_optim=False)
 #
 #     OptimalControlProgram.save(ocp, sol, "../Results/jumper2phases_sol")
 
 
 if __name__ == "__main__":
-    model_path = "../models/jumper2contacts.bioMod"
-    # time_min = 0.4
-    # time_max = 0.75
-    phase_time = 0.32
+    model_path = (
+        "../models/jumper2contacts.bioMod",
+    )
+    # time_min = 0.2
+    # time_max = 1
+    phase_time = 0.4
     number_shooting_points = 30
 
     tic = time()
@@ -215,22 +266,29 @@ if __name__ == "__main__":
         phase_time=phase_time,
         number_shooting_points=number_shooting_points,
         use_symmetry=True,
-        use_actuators=False,
+        use_actuators=True,
     )
-    ocp.add_plot("CoM", lambda x, u, p: plot_CoM(x), phase_number=0, plot_type=PlotType.PLOT)
-    ocp.add_plot("CoM_dot", lambda x, u, p: plot_CoM_dot(x), phase_number=0, plot_type=PlotType.PLOT)
+    ocp.add_plot(
+        "CoM", lambda x, u, p: plot_CoM(x, "../models/jumper2contacts.bioMod"), phase_number=0, plot_type=PlotType.PLOT
+    )
+    ocp.add_plot(
+        "CoM_dot",
+        lambda x, u, p: plot_CoM_dot(x, "../models/jumper2contacts.bioMod"),
+        phase_number=0,
+        plot_type=PlotType.PLOT,
+    )
 
-    sol = ocp.solve(show_online_optim=True, options_ipopt={"hessian_approximation": "exact", "max_iter": 1000})
+    sol = ocp.solve(show_online_optim=True, solver_options={"hessian_approximation": "exact", "max_iter": 10000})
 
-    # --- Show results --- #
+    #--- Show results --- #
     # param = Data.get_data(ocp, sol["x"], get_states=False, get_controls=False, get_parameters=True)
     # print(
-    #     f"The optimized phases times are: {param['time'][0, 0]}s."
+    #     f"The optimized phase time are: {param['time'][0, 0]}s."
     # )
 
     toc = time() - tic
     print(f"Time to solve : {toc}sec")
 
     result = ShowResult(ocp, sol)
-    result.graphs()
+    result.graphs(adapt_graph_size_to_bounds=False)
     result.animate(nb_frames=61)
